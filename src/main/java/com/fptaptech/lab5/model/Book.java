259e48c963d0bd8374a3238162c59bffd98f3735
tuan.cat <tuan.cat@mservice.com.vn>
2022-03-24 15:29:28 +0700

    - update

---
+++ src/main/java/com/fptaptech/lab5/model/Book.java
@@ -0,0 +1,28 @@
+package com.fptaptech.lab5.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "book")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "author", nullable = false)
+    private String author;
+    @Column(name = "description", nullable = false)
+    private String description;
+}
